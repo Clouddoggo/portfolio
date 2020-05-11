235b8d0ccc41edd82771cb5ace037f9cf34c7b66
Jel <42993921+Clouddoggo@users.noreply.github.com>
2020-05-11 16:02:05 +0800

    Updates

---
+++ precache-manifest.bfc74dfd8e7e8613963aabfa339b4672.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed59efad98612f5f67562f3c2ba820b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "351c502baf780e56d430",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c589f15c8131bfc593b1",
+    "url": "/portfolio/static/css/main.5f12e663.chunk.css"
+  },
+  {
+    "revision": "351c502baf780e56d430",
+    "url": "/portfolio/static/js/2.d348b174.chunk.js"
+  },
+  {
+    "revision": "1cb34fcc5e9ef14c6feea7d1cd9b4db7",
+    "url": "/portfolio/static/js/2.d348b174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c589f15c8131bfc593b1",
+    "url": "/portfolio/static/js/main.de9047e0.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "8ed92b3bd20593e67db6ef84d497a370",
+    "url": "/portfolio/static/media/no-copyright.8ed92b3b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.53fd300bac438cccd082300a6cb5cb90.js"
+  "/portfolio/precache-manifest.bfc74dfd8e7e8613963aabfa339b4672.js"
 );
 
 self.addEventListener('message', (event) => {
