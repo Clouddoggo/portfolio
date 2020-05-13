12d7aab390f87036e683fcfb0b70f73abef64f1c
Jel <42993921+Clouddoggo@users.noreply.github.com>
2020-05-13 19:05:56 +0800

    add modals for each project

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
+// TODO: add checks for required fields & check submit button working
 
 class Contact extends Component {
 
@@ -13,40 +15,43 @@ class Contact extends Component {
 
     changeHandler = (event) => {
         this.setState({ message: event.target.value });
+        console.log("message: " + event.target.value);
     }
 
     submitHandler = (event) => {
         event.preventDefault();
-        alert("You are submitting " + this.state.username);
+        alert("You are submitting " + this.state.message);
     }
 
     render() {
         return (
-            <Container id="contact">
-                <Form>
-                    <Form.Group controlId="formGroupEmail">
-                        <Form.Label class="float-left">Email address (required)</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-                    <Form.Group controlId="formGroupName">
-                        <Form.Row>
-                            <Col>
-                                <Form.Label class="float-left">First name (required)</Form.Label>
-                                <Form.Control placeholder="First name" />
-                            </Col>
-                            <Col>
-                                <Form.Label class="float-left">Last name (required)</Form.Label>
-                                <Form.Control placeholder="Last name" />
-                            </Col>
-                        </Form.Row>
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                        <Form.Label class="float-left">Your message</Form.Label>
-                        <Form.Control as="textarea" rows="4" onChange={this.changeHandler} />
-                    </Form.Group>
-                    <Button class="btn-primary float-left" type="submit" onSubmit={this.submitHandler}>Submit</Button>
-                </Form>
-            </Container>
+            <section id="contact">
+                <Container>
+                    <Form>
+                        <Form.Group controlId="formGroupEmail">
+                            <Form.Label class="float-left">Email address (required)</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group controlId="formGroupName">
+                            <Form.Row>
+                                <Col>
+                                    <Form.Label class="float-left">First name (required)</Form.Label>
+                                    <Form.Control placeholder="First name" />
+                                </Col>
+                                <Col>
+                                    <Form.Label class="float-left">Last name (required)</Form.Label>
+                                    <Form.Control placeholder="Last name" />
+                                </Col>
+                            </Form.Row>
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label class="float-left">Your message (required)</Form.Label>
+                            <Form.Control as="textarea" rows="4" onChange={this.changeHandler} />
+                        </Form.Group>
+                        <Button class="btn-primary" type="submit" onSubmit={this.submitHandler}>Submit</Button>
+                    </Form>
+                </Container>
+            </section>
         );
     }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,24 +2,46 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class Projects extends Component {
+
     render() {
         // To switch to data file
         // let data = this.props.data;
 
         return (
             <section id="projects">
-                <Container id="projects-container d-flex justify-content-center">
+                <Container class="d-flex justify-content-center" fluid>
                     <Row>
                         <div class="card-group align-items-stretch col-lg-12">
                             <Card>
                                 <div class="card-body">
                                     <h4 class="card-title">Todo App</h4>
                                     <p class="card-text">
-                                        Heroku deployment in progress
+                                        test
                                     </p>
-                                    <a href="#!" class="btn btn-primary">Learn more</a>
+                                    <Button class="btn btn-primary" data-toggle="modal" data-target="#todoModal">
+                                        Learn more
+                                    </Button>
+                                    <div class="modal" id="todoModal" tabindex="-1" role="dialog" aria-labelledby="todoModal" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="todoModal">Todo App</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Heroku deployment in progress
+      </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <a href="https://github.com/Clouddoggo/todo-app" target="_blank" class="btn btn-primary">Source code</a>
                                 </div>
                             </Card>
@@ -27,14 +49,33 @@ class Projects extends Component {
                                 <div class="card-body">
                                     <h4 class="card-title">Duke</h4>
                                     <p class="card-text">
-                                        Individual project for CS2103T Software Engineering
-                                    <ul>
-                                            <li>Worked on basic commands such as add, delete, find and list tasks</li>
-                                            <li>Added help command for users to view list of commands available</li>
-                                            <li>Wrote a comprehensive User Guide for new and existing users</li>
-                                        </ul>
+                                        test
                                     </p>
-                                    <a href="#!" class="btn btn-primary">Learn more</a>
+                                    <Button class="btn btn-primary" data-toggle="modal" data-target="#dukeModal">
+                                        Learn more
+                                    </Button>
+                                    <div class="modal" id="dukeModal" tabindex="-1" role="dialog" aria-labelledby="dukeModal" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="dukeModal">Individual project for CS2103T Software Engineering</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <ul>
+                                                        <li>Worked on basic commands such as add, delete, find and list tasks</li>
+                                                        <li>Added help command for users to view list of commands available</li>
+                                                        <li>Wrote a comprehensive User Guide for new and existing users</li>
+                                                    </ul>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <a href="https://github.com/Clouddoggo/duke" target="_blank" class="btn btn-primary">Source code</a>
                                 </div>
                             </Card>
@@ -46,16 +87,35 @@ class Projects extends Component {
                                 <div class="card-body">
                                     <h4 class="card-title">JelphaBot</h4>
                                     <p class="card-text">
-                                        Team project for CS2103T Software Engineering.
-                                        Worked on the productivity aspect for users
-                                        <ul>
-                                            <li><code>start</code> and <code>stop</code> timer commands</li>
-                                            <li>Switch tab command: productivity, :p and :P</li>
-                                            <li>Rendering of productivity panel</li>
-                                        </ul>
+                                        test
+                                    </p>
+                                    <Button class="btn btn-primary" data-toggle="modal" data-target="#jelphabotModal">
+                                        Learn more
+                                    </Button>
+                                    <div class="modal" id="jelphabotModal" tabindex="-1" role="dialog" aria-labelledby="jelphabotModal" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="jelphabotModal">Team project for CS2103T Software Engineering</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <ul>
+                                                        <li>In charge of the productivity aspect of app</li>
+                                                        <li><code>start</code> and <code>stop</code> timer commands</li>
+                                                        <li>Switch tab command: productivity, :p and :P</li>
+                                                        <li>Rendering of productivity panel</li>
+                                                    </ul>
                                         Also worked on morphing the existing address book into a task manager by adding the DateTime class and test cases
-    </p>
-                                    <a href="#!" class="btn btn-primary">Learn more</a>
+      </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <a href="https://github.com/Clouddoggo/main" target="_blank" class="btn btn-primary">Source code</a>
                                 </div>
                             </Card>
@@ -63,10 +123,30 @@ class Projects extends Component {
                                 <div class="card-body">
                                     <h4 class="card-title">This Website</h4>
                                     <p class="card-text">
-                                        Some quick example text to build on the card title
-                                        and make up the bulk of the card's content.
-    </p>
-                                    <a href="#!" class="btn btn-primary">Learn more</a>
+                                        test
+                                    </p>
+                                    <Button class="btn btn-primary" data-toggle="modal" data-target="#websiteModal">
+                                        Learn more
+                                    </Button>
+                                    <div class="modal" id="websiteModal" tabindex="-1" role="dialog" aria-labelledby="websiteModal" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="websiteModal">This porfolio</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Some quick example text to build on the card title
+                                                    and make up the bulk of the card's content.
+      </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <a href="https://github.com/Clouddoggo/portfolio" target="_blank" class="btn btn-primary">Source code</a>
                                 </div>
                             </Card>
