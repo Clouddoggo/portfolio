f58d1b3aa81ad0c186e38139d9049480ea3ee5bc
Jel <42993921+Clouddoggo@users.noreply.github.com>
2020-12-07 20:53:32 +0800

    Updates

--- precache-manifest.576c292f6d42d6d7929fe0e821fbe0b2.js
+++ precache-manifest.576c292f6d42d6d7929fe0e821fbe0b2.js
@@ -1,43 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d455a1a9ce7cceb2a6d8833357cc09ec",
+    "revision": "3ced98b125bd2fe298925342f080e6db",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "7585943d32fa813cdfde",
+    "revision": "0b86d260a7c4a976d16d",
     "url": "/portfolio/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "396cf4f661fe253d4885",
-    "url": "/portfolio/static/css/main.0583a150.chunk.css"
+    "revision": "4c576418e131ea162b25",
+    "url": "/portfolio/static/css/main.1131167e.chunk.css"
   },
   {
-    "revision": "7585943d32fa813cdfde",
-    "url": "/portfolio/static/js/2.0098faf9.chunk.js"
+    "revision": "0b86d260a7c4a976d16d",
+    "url": "/portfolio/static/js/2.edd4196a.chunk.js"
   },
   {
     "revision": "11fd504c3f9976bd574e4ee6f682500d",
-    "url": "/portfolio/static/js/2.0098faf9.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.edd4196a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "396cf4f661fe253d4885",
-    "url": "/portfolio/static/js/main.f48dd624.chunk.js"
+    "revision": "4c576418e131ea162b25",
+    "url": "/portfolio/static/js/main.cd338585.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
     "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
-  },
-  {
-    "revision": "bb25a45b577fbe3be3819778efd665f5",
-    "url": "/portfolio/static/media/ChairVise.bb25a45b.png"
-  },
-  {
-    "revision": "fc8e3fcd5189d8d2905272dcf70fae4c",
-    "url": "/portfolio/static/media/Duke.fc8e3fcd.png"
-  },
-  {
-    "revision": "5c6ac250d04c5b3ed9ddb2066f8fb3c9",
-    "url": "/portfolio/static/media/JelphaBot.5c6ac250.png"
   },
   {
     "revision": "324a513cb9c683dc30777bdbccf59995",
@@ -56,10 +44,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/Raleway-Medium.bb5ae98e.ttf"
   },
   {
-    "revision": "b807164b43b5edad029ea69a55a0b963",
-    "url": "/portfolio/static/media/TaskListTab.b807164b.png"
-  },
-  {
     "revision": "13b202f749e4a3a95d8c21458c3a8da6",
     "url": "/portfolio/static/media/about.13b202f7.svg"
   },
@@ -68,7 +52,27 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/media/card.a88240e6.png"
   },
   {
+    "revision": "f49be9c25f6938d2538a1f473e74d387",
+    "url": "/portfolio/static/media/chair.f49be9c2.png"
+  },
+  {
+    "revision": "c862babc5f084bfd3c7b62acc47bf3fc",
+    "url": "/portfolio/static/media/chairvise.c862babc.png"
+  },
+  {
     "revision": "48c138f681fe69d2eb9834d3afb4d447",
     "url": "/portfolio/static/media/clouddoggo.48c138f6.png"
+  },
+  {
+    "revision": "fd6e622c1534e7d1240bfb244396c50f",
+    "url": "/portfolio/static/media/duke.fd6e622c.png"
+  },
+  {
+    "revision": "b184be72c65313ec69638caec5af7c3a",
+    "url": "/portfolio/static/media/jelpha.b184be72.png"
+  },
+  {
+    "revision": "e02caad2c57ee69ccf4bfb91e3a5faaf",
+    "url": "/portfolio/static/media/jelphabot.e02caad2.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.03a5b088a203402f9bc9ec550f04b52f.js"
+  "/portfolio/precache-manifest.576c292f6d42d6d7929fe0e821fbe0b2.js"
 );
 
 self.addEventListener('message', (event) => {
